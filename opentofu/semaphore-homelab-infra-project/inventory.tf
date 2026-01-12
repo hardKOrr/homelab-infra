0ff431f329d76a507f707ed94c58111a9b7e0243
Kevin Orr <korrx13@hotmail.com>
2026-01-12 16:29:08 -0600

    ok we're done here

--- opentofu/semaphore-homelab-infra-project/inventory.tf
+++
@@ -1,9 +0,0 @@
-resource "semaphoreui_project_inventory" "homelab_infra_workspace" {
-  project_id = semaphoreui_project.homelab_infra.id
-  name       = "Homelab Infra Project Workspace"
-  ssh_key_id = semaphoreui_project_key.none.id
-
-  terraform_workspace = {
-    workspace = "homelab-infra-project"
-  }
-}
--- opentofu/semaphore-homelab-infra-project/variable_groups.tf
+++
@@ -1,4 +0,0 @@
-resource "semaphoreui_project_environment" "homelab_infra_variable_group" {
-  project_id = semaphoreui_project.homelab_infra.id
-  name       = "Homelab Infra OpenTofu Environment"
-}
