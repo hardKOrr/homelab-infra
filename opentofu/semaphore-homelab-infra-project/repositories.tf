7b42655bbe2cc44251dfabe1ee210ced3ff1337f
hardKOrr <kdorr13@gmail.com>
2026-01-12 16:36:34 -0600

    ok we're done here (#7)
    Co-authored-by: Kevin Orr <korrx13@hotmail.com>

--- opentofu/semaphore-homelab-infra-project/repositories.tf
+++
@@ -1,8 +0,0 @@
-# Repository hooked to this project
-resource "semaphoreui_project_repository" "homelab_infra" {
-  project_id = semaphoreui_project.homelab_infra.id
-  name       = "Github hardKOrr/homelab-infra"
-  url        = "git@github.com:hardKOrr/homelab-infra.git"
-  branch     = "master"
-  ssh_key_id = semaphoreui_project_key.github.id
-}
